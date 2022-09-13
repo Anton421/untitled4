aeda9af67e4c68afed6c3b98f9117e88085b96e4
Anton421 <112985165+Anton421@users.noreply.github.com>
2022-09-14 01:56:50 +0300

    Initial commit

---
+++ src/Truba.java
@@ -0,0 +1,23 @@
+public class Truba implements Instrument{
+    private String  diametr ="12";
+
+    public String getDiametr() {
+        return diametr;
+    }
+
+    public void setDiametr(String diametr) {
+        this.diametr = diametr;
+    }
+
+    @Override
+    public void play() {
+        System.out.println("haracteristic " + getDiametr());
+    }
+
+    @Override
+    public String toString() {
+        return "Truba{" +
+                "diametr=" + diametr +
+                '}';
+    }
+}
